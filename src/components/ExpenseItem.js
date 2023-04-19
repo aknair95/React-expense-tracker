df5ca4763148a8f9fdcbf9fa615e9dea59e01ac5
aknair95 <aakashunair01031995@gmail.com>
2023-04-20 04:23:00 +0530

    CSS styling added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,14 @@
+import './ExpenseItem.css';
 
 function ExpenseItem(){
     return (
-        <div>
-            <h2>Expense Items-</h2>
-            <p>Food Rs 10</p>
-            <p>Petrol Rs 100</p>
-            <p>Movies Rs 200</p>
+        <div className="expense-item">
+            <h2 className='expense-item h2'>Expense Items-</h2>
+            <div className="expense-item__description">       
+               <div>Food</div> <div className='expense-item__price'>Rs 10</div>
+               <div>Petrol</div> <div className='expense-item__price'>Rs 100</div>
+               <div>Movies</div> <div className='expense-item__price'>Rs 200</div>
+            </div>
         </div>
     )
 }
