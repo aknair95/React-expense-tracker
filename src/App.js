5a41968fb1df7906523515020811a878c147178a
aknair95 <aakashunair01031995@gmail.com>
2023-04-20 06:26:02 +0530

    passing data using props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import ExpenseItem from "./components/ExpenseItem";
 
+import './components/ExpenseItem.css';
+
 function App() {
+  const expenses=[
+    {
+      title: "Food",
+      amount: "Rs 10",
+      location: "Delhi"
+    },
+    {
+      title: "Movie",
+      amount: "Rs 100",
+      location: "Mumbai"
+    },
+    {
+      title: "Petrol",
+      amount: "Rs 1000",
+      location: "Agra"
+    }
+  ];
   return (
     <div className="App">
       <header className="App-header">
+      <h2 className='expense-item h2'>Expense Items-</h2>
         <p>
-          <ExpenseItem></ExpenseItem>
+          <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} location={expenses[0].location}></ExpenseItem>
+          <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} location={expenses[1].location}></ExpenseItem>
+          <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} location={expenses[2].location}></ExpenseItem>
         </p>
       </header>
     </div>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,11 @@
 
 import './ExpenseItem.css';
 
-function ExpenseItem(){
-    const expenseItem1="Food";
-    const expenseItem2="Petrol";
-    const expenseItem3="Movies";
-    const expensePrice1="Rs 10";
-    const expensePrice2="Rs 100";
-    const expensePrice3="Rs 200";
-    const LocationOfExpenditure1="Delhi";
-    const LocationOfExpenditure2="Bhopal";
-    const LocationOfExpenditure3="Mumbai";
-
+function ExpenseItem(props){
     return (
         <div className="expense-item">
-            <h2 className='expense-item h2'>Expense Items-</h2>
             <div className="expense-item__description">    
-                <div>{expenseItem1}</div><div className='expense-item__price'>{expensePrice1}</div> <div>{LocationOfExpenditure1}</div>
-                <div>{expenseItem2}</div> <div className='expense-item__price'>{expensePrice2}</div> <div>{LocationOfExpenditure2}</div>
-                <div>{expenseItem3}</div> <div className='expense-item__price'>{expensePrice3}</div> <div>{LocationOfExpenditure3}</div> 
+                <div>{props.title}</div><div className='expense-item__price'>{props.amount}</div> <div>{props.location}</div> 
             </div>     
         </div>
     )
