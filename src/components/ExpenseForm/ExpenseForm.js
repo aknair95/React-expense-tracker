6be8a95c54c9cb16f75933f461d76e0858c44cd7
aknair95 <aakashunair01031995@gmail.com>
2023-04-26 03:26:42 +0530

    states introduced for form data fetching

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 let ExpenseForm=() =>{
 
+    const [enteredDate,setDate]= useState("");
+    const [enteredTitle,setTitle]= useState("");
+    const [enteredAmount,setAmount]= useState("");
+
     let addExpense=(e) =>{
         e.preventDefault();
-        console.log("Expense Added!!!");
+        console.log("Expense added !!!");
     }
 
-    let print= (e) =>{
-        console.log(e.target.value);
+    let addDate= (e1) =>{
+        setDate(e1.target.value);
+    }
+
+    let addTitle= (e2) =>{
+        setTitle(e2.target.value);
+    }
+
+    let addAmount= (e3) =>{
+        setAmount(e3.target.value);
     }
 
     return(
@@ -16,15 +29,15 @@ let ExpenseForm=() =>{
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Date-</label>
-                    <input type="date" onChange={print}/>
+                    <input type="date" onChange={addDate}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Title-</label>
-                    <input type="text" onChange={print}/>
+                    <input type="text" onChange={addTitle}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount-</label>
-                    <input type="number" onChange={print}/>
+                    <input type="number" onChange={addAmount}/>
                 </div>
             </div>
             <div className="new-expense__actions">
